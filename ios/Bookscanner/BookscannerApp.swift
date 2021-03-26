06ab28082ee030571a5282c29ed2e5af17a0c9f3
Luciano Nooijen <luciano@bytecode.nl>
2021-03-26 10:29:25 +0100

    Init iOS Swift project
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ ios/Bookscanner/BookscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookscannerApp.swift
+//  Bookscanner
+//
+//  Created by Luciano Nooijen on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
