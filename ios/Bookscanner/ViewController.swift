3a33a3a9da6a757a22896c6afaf648b55d374e0b
Luciano Nooijen <luciano@bytecode.nl>
2021-03-26 12:50:00 +0100

    Re-init project but with Storyboard
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ ios/Bookscanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookscanner
+//
+//  Created by Luciano Nooijen on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
